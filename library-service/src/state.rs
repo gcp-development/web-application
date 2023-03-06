0083a739d9ee935ff23c1cac932f97572d79ea8a
gcp-development <bruno.silveira.backup@gmail.com>
2023-03-06 11:20:30 +0000

    project-structure

--- library-service/src/state.rs
+++ library-service/src/state.rs
@@ -1,9 +1,7 @@
 use std::sync::Mutex;
 use sqlx::postgres::PgPool;
-use crate::model::book::Book;
 
 pub struct AppState {
     pub probe: String,
-    pub library: Mutex<Vec<Book>>,
     pub db: PgPool,
 }
