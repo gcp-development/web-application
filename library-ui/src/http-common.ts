ab79082d5a47c89b5afd029c039a7fa5a32266b6
gcp-development <bruno.silveira.backup@gmail.com>
2023-03-08 17:31:22 +0000

    base-ui

--- library-ui/src/http-common.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://demo:31726/",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
--- library-ui/src/types/Book.ts
+++
@@ -1,6 +0,0 @@
-export default interface IBook {
-    id: number,
-    title: string,
-    author: string,
-    posted_time: string,
-  }
