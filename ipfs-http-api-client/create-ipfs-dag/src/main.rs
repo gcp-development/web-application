8a905f99044de38293aaaf24db96587f0d680bc5
gcp-development <bruno.silveira.backup@gmail.com>
2023-03-20 12:21:37 +0000

    create-ipfs-dag

--- ipfs-http-api-client/create-ipfs-dag/src/main.rs
+++ ipfs-http-api-client/create-ipfs-dag/src/main.rs
@@ -1,21 +1,14 @@
-use std::io;
-use std::io::{Cursor, Write};
+use std::io::{Cursor};
 use futures::TryStreamExt;
 use ipfs_api_backend_actix::{IpfsApi, IpfsClient, TryFromUri};
-use ipfs_api_backend_actix::request::{DagCodec, DagPut};
-
 
 static IPFS_API: &str = "http://demo:32546/";
-static CID_BOOK1: &str = "QmTN78XgBo6fPaWrDhsPf6yzJkcuqpEUBqVRtHu3i5yosL";
-static CID_BOOK2: &str = "QmYqo1Ack8g2rDX6TEoPA14oNASJrXEVB4oTEKv8So6Ect";
-static CID_BOOK3: &str = "QmUfV4m2PUM559LSvDsJkoz1KofTVq25RDXwW5uMdjNb4u";
 
 #[actix_rt::main]
 async fn main() {
     let client = IpfsClient::from_str(&IPFS_API)
         .unwrap();
 
-//r#"[{"/":"QmTN78XgBo6fPaWrDhsPf6yzJkcuqpEUBqVRtHu3i5yosL","name":[{"/":"QmTN78XgBo6fPaWrDhsPf6yzJkcuqpEUBqVRtHu3i5yosL"}]}]"#
     let dag_node = Cursor::new(r#"[{ "/":"QmTN78XgBo6fPaWrDhsPf6yzJkcuqpEUBqVRtHu3i5yosL"},{ "/":"QmYqo1Ack8g2rDX6TEoPA14oNASJrXEVB4oTEKv8So6Ect"},{"/":"QmUfV4m2PUM559LSvDsJkoz1KofTVq25RDXwW5uMdjNb4u"}]"#);
 
     let response = client
